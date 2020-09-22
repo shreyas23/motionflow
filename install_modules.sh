38a68bb58cc29fbf53a14fe467273d84a31c70f1
Shreyas Niradi <shreyas.niradi@gmail.com>
2020-09-21 20:33:51 -0700

    update python command

--- install_modules.sh
+++ install_modules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ./models/correlation_package
-python setup.py install
+python3 setup.py install
 cd ../forwardwarp_package
-python setup.py install
+python3 setup.py install
 cd ../..
