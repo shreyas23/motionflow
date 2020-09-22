3e5eaba162ff855424c140875f273315b69164b9
Shreyas Niradi <shreyas.niradi@gmail.com>
2020-09-21 19:42:32 -0700

    update pip command

--- env_setup.sh
+++ env_setup.sh
@@ -1,5 +1,5 @@
-pip3 install -r requirements.txt;
-chmod u+x install_modules.sh;
-./install_modules;
-chmod u+x run.sh;
+python3 -m pip install -r requirements.txt &&
+chmod u+x install_modules.sh &&
+./install_modules &&
+chmod u+x run.sh &&
 ./run.sh;
