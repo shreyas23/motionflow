02087c62008eca87123bf955c77277937c320713
Shreyas Niradi <shreyas.niradi@gmail.com>
2020-09-27 04:41:35 -0700

    update run params

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python3 train.py --data_root=/ceph/kitti_jpg/ --exp_name="scenenet-cons" --num_examples=500 --resize_only=True --model_name="scenenet" --epochs=50 --batch_size=4 --log_dir="/ceph/checkpoints/" --num_workers=8 --lr_sched_type='step' --lr=2e-4 --use_mask=True
+python3 train.py --data_root=/ceph/kitti_jpg/ --exp_name="scenenet-cons" --num_examples=1000 --resize_only=True --model_name="scenenet" --epochs=50 --batch_size=4 --log_dir="/ceph/checkpoints/" --num_workers=8 --lr_sched_type='step' --lr=2e-4 --use_mask=True
