4ab1f53cb629586d6763f4b0e396f976f4ffb98c
Shreyas Niradi <shreyas.niradi@gmail.com>
2020-09-27 01:15:57 -0700

    update run params

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python3 train.py --data_root=/ceph/kitti_jpg/ --exp_name="overfit" --num_examples=-1 --resize_only=True --model_name="scenenet" --epochs=50 --batch_size=4 --log_dir="/ceph/checkpoints/" --num_workers=16 --lr_sched_type='step' --lr=2e-4 --use_mask=True
+python3 train.py --data_root=/ceph/kitti_jpg/ --exp_name="scenenet-cons" --num_examples=500 --resize_only=True --model_name="scenenet" --epochs=50 --batch_size=4 --log_dir="/ceph/checkpoints/" --num_workers=8 --lr_sched_type='step' --lr=2e-4 --use_mask=True
