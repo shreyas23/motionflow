dc9d8398ceddc25c655b32563a5897c8999448e3
Shreyas Niradi <shreyas.niradi@gmail.com>
2020-10-08 19:30:01 -0700

    update env setup

--- scripts/env_setup_dist.sh
+++ scripts/env_setup_dist.sh
@@ -1,5 +1,5 @@
 pip3 install -r requirements.txt &&
-chmod u+x install_modules.sh &&
-./install_modules.sh &&
-chmod u+x run_dist.sh &&
-./run_dist.sh;
+chmod u+x ./scripts/install_modules.sh &&
+./scripts/install_modules.sh &&
+chmod u+x ./scripts/run_dist.sh &&
+./scripts/run_dist.sh;
