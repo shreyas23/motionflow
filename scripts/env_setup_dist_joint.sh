c5aa5f040187110ac497f1aa5b6918d28d6f5679
Shreyas Niradi <shreyas.niradi@gmail.com>
2020-10-11 00:33:27 -0700

    fix typo

---
+++ scripts/env_setup_dist_joint.sh
@@ -0,0 +1,5 @@
+pip3 install -r requirements.txt &&
+chmod u+x ./scripts/install_modules.sh &&
+./scripts/install_modules.sh &&
+chmod u+x ./scripts/run_dist_joint.sh &&
+./scripts/run_dist_joint.sh;
