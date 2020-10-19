dd70e736be285c7ea785464036f9cd25c9830ae1
Shreyas Niradi <shreyas.niradi@gmail.com>
2020-10-18 21:01:36 -0700

    update env setup

--- scripts/env_setup_dist_joint.sh
+++ scripts/env_setup_dist_joint.sh
@@ -1,5 +1,5 @@
 pip3 install -r requirements.txt &&
 chmod u+x ./scripts/install_modules.sh &&
 ./scripts/install_modules.sh &&
-chmod u+x ./scripts/run_dist_joint.sh &&
-./scripts/run_dist_joint.sh;
+chmod u+x ./scripts/train_baseline_joint.sh &&
+./scripts/train_baseline_joint.sh;
