2dbbbca14cf6897caa56e047ee87920754747236
Shreyas Niradi <shreyas.niradi@gmail.com>
2020-10-23 15:49:45 -0700

    try building from source

--- scripts/install_modules.sh
+++ scripts/install_modules.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-pip install spatial-correlation-sampler
-pip3 install spatial-correlation-sampler
+# pip install spatial-correlation-sampler
+# pip3 install spatial-correlation-sampler
+git clone https://github.com/ClementPinard/Pytorch-Correlation-extension.git
+python3 ./Pytorch-Correlation-extension/setup.py
 cd ./models/correlation_package
 python3 setup.py install
 cd ../forwardwarp_package
