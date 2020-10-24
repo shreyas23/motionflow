dd7746414e914afe5f40dfac42c3a7119a6b0ea0
Shreyas Niradi <shreyas.niradi@gmail.com>
2020-10-23 20:58:12 -0700

    fixed correlation issue

--- scripts/install_modules.sh
+++ scripts/install_modules.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pip3 install spatial-correlation-sampler -vvv
+pip3 install spatial-correlation-sampler==0.2.1
 cd ./models/correlation_package
 python3 setup.py install
 cd ../forwardwarp_package
