69b30fc7791a50fc56901668f3f2341689e2c60a
Shreyas Niradi <shreyas.niradi@gmail.com>
2020-10-23 18:06:09 -0700

    add verbose

--- scripts/install_modules.sh
+++ scripts/install_modules.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pip3 install spatial-correlation-sampler
+pip3 install spatial-correlation-sampler -vvv
 git clone https://github.com/ClementPinard/Pytorch-Correlation-extension.git
 cd Pytorch-Correlation-extension/
 python setup.py install
