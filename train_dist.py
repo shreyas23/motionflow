366c90f492f936986f4d34eab6be2b7c7d8d55c4
Shreyas Niradi <shreyas.niradi@gmail.com>
2020-10-26 21:40:07 -0700

    update scripts

--- train_dist.py
+++ train_dist.py
@@ -255,7 +255,7 @@ def train(gpu, args):
             optimizer, factor=args.lr_gamma, verbose=True, mode='min', patience=1)
     elif args.lr_sched_type == 'step':
         print("Using step lr schedule")
-        milestones = [10, 13]
+        milestones = [15, 20, 30]
         lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
             optimizer, milestones=milestones, gamma=args.lr_gamma)
     elif args.lr_sched_type == 'none':
