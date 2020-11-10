72fecae8bcf2bada5cc09bdd649e23b6633f1440
Shreyas Niradi <shreyas.niradi@gmail.com>
2020-11-10 11:39:29 -0800

    final pose fix bc im stupid

--- utils/sceneflow_util.py
+++ utils/sceneflow_util.py
@@ -12,7 +12,8 @@ from sys import exit
 def add_pose(pose_mat, pose_res):
     b, _, _ = pose_mat.shape
     pose_mat_res = pose_vec2mat(pose_res)
-    pose_mat_full = torch.cat([pose_mat, torch.ones(b, 1, 4).cuda()], dim=1)
+    pose_mat_full = torch.cat([pose_mat, torch.zeros(b, 1, 4).to(device=pose_mat.device)], dim=1)
+    pose_mat_full[:, -1, -1] = 1
     return torch.bmm(pose_mat_res, pose_mat_full)
 
 
