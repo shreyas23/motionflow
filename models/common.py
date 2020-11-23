need to fix vanishing gradient

models/common.py
@@ -23,12 +23,14 @@ class UpConv(nn.Module):
 class Conv(nn.Module):
     """ Conv layer with options for padding and ELU/LeakyReLU nonlinearity
     """
-    def __init__(self, in_chs, out_chs, kernel_size=3, stride=1, dilation=1, nonlin='elu', pad_mode='reflection', use_bn=False, bias=True):
+    def __init__(self, in_chs, out_chs, kernel_size=3, stride=1, dilation=1, nonlin='leakyrelu', pad_mode='reflection', use_bn=False, bias=True):
+    # def __init__(self, in_chs, out_chs, kernel_size=3, stride=1, dilation=1, nonlin='elu', pad_mode='reflection', use_bn=False, bias=True):
         super(Conv, self).__init__()
 
         nonlin_dict = {
             'leakyrelu' : nn.LeakyReLU(0.1, inplace=True),
             'elu': nn.ELU(inplace=True),
+            'relu': nn.ReLU(inplace=True)
         }
 
         padding = ((kernel_size - 1) * dilation) // 2

models/disp_decoders.py
@@ -26,14 +26,14 @@ class DispDecoder(nn.Module):
             # upconv_0
             num_ch_in = self.num_ch_enc[-1] if i == 4 else self.num_ch_dec[i + 1]
             num_ch_out = self.num_ch_dec[i]
-            self.convs[("upconv", i, 0)] = Conv(num_ch_in, num_ch_out)
+            self.convs[("upconv", i, 0)] = Conv(num_ch_in, num_ch_out, nonlin='leakyrelu')
 
             # upconv_1
             num_ch_in = self.num_ch_dec[i]
             if self.use_skips and i > 0:
                 num_ch_in += self.num_ch_enc[i-1]
             num_ch_out = self.num_ch_dec[i]
-            self.convs[("upconv", i, 1)] = Conv(num_ch_in, num_ch_out)
+            self.convs[("upconv", i, 1)] = Conv(num_ch_in, num_ch_out, nonlin='leakyrelu')
 
         for s in self.scales:
             self.convs[("dispconv", s)] = Conv(self.num_ch_dec[s], self.num_output_channels, nonlin='none')
@@ -41,12 +41,13 @@ class DispDecoder(nn.Module):
         self.decoder = nn.ModuleList(list(self.convs.values()))
         self.sigmoid = nn.Sigmoid()
 
-        # self.init_weights()
+        self.init_weights()
 
     def init_weights(self):
         for layer in self.modules():
             if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.ConvTranspose2d):
                 nn.init.kaiming_normal_(layer.weight)
+                # nn.init.xavier_normal_(layer.weight)
                 if layer.bias is not None:
                     nn.init.constant_(layer.bias, 0)
 
@@ -74,7 +75,8 @@ class DispDecoder(nn.Module):
             x = self.convs[("upconv", i, 1)](x)
 
             if i in self.scales:
-                out_disp = self.sigmoid(self.convs[("dispconv", i)](x))
+                out_disp = self.sigmoid(self.convs[("dispconv", i)](x)) * 0.3
+                out_disp.register_hook(lambda grad: print("out_disp", grad.mean()))
                 self.outputs.append(out_disp)
 
         return self.outputs

new_losses.py
@@ -141,6 +141,14 @@ class Loss(nn.Module):
                 if self.args.use_flow_mask:
                     flow_mask_l1 = 1.0 - mask_l1
                     flow_mask_l2 = 1.0 - mask_l2
+                else:
+                    flow_mask_l1 = None
+                    flow_mask_l2 = None
+            else:
+                mask_l1 = None
+                mask_l2 = None
+                flow_mask_l1 = None
+                flow_mask_l2 = None
 
             # depth diffs
             disp_diff1, left_occ1, loss_disp_sm1 = self.depth_loss(disp_l1, disp_r1, img_l1, img_r1, s)
@@ -164,6 +172,11 @@ class Loss(nn.Module):
             mask_disp_diff1 = (disp_diff1 <= min_flow_diff1).detach()
             mask_disp_diff2 = (disp_diff2 <= min_flow_diff2).detach()
 
+            # print("-----------------")
+            # print(mask_disp_diff1.sum())
+            # print(mask_disp_diff2.sum())
+            # print("-----------------")
+
             if mask_disp_diff1.sum() == 0:
                 mask_disp_diff1 = torch.ones_like(mask_disp_diff1).detach()
             
@@ -176,19 +189,25 @@ class Loss(nn.Module):
             depth_loss2 = disp_diff2[left_occ2].mean()
             depth_loss = depth_loss1 + depth_loss2
 
+            occ_f = pose_occ_f * sf_occ_f * left_occ1
+            occ_b = pose_occ_b * sf_occ_b * left_occ2
+
+            if self.args.use_mask:
+                occ_f = occ_f * mask_l1
+                occ_b = occ_b * mask_l2
+
             if self.flow_reduce_mode == 'min':
-                occ_f = pose_occ_f * sf_occ_f * left_occ1
-                occ_b = pose_occ_b * sf_occ_b * left_occ2
                 flow_loss1 = min_flow_diff1[occ_f].mean()
                 flow_loss2 = min_flow_diff2[occ_b].mean()
             elif self.flow_reduce_mode == 'avg':
-                occ_f = pose_occ_f * sf_occ_f * left_occ1
-                occ_b = pose_occ_b * sf_occ_b * left_occ2
                 flow_loss1 = flow_diffs1.mean(dim=1, keepdim=True)[occ_f].mean()
                 flow_loss2 = flow_diffs2.mean(dim=1, keepdim=True)[occ_b].mean()
             elif self.flow_reduce_mode == 'sum':
                 occ_f = torch.cat([pose_occ_f, sf_occ_f], dim=1) * left_occ1
                 occ_b = torch.cat([pose_occ_b, sf_occ_b], dim=1) * left_occ2
+                if self.args.use_mask:
+                    occ_f = occ_f * mask_l1
+                    occ_b = occ_b * mask_l2
                 flow_loss1 = (flow_diffs1 * occ_f.float()).sum(dim=1, keepdim=True).mean()
                 flow_loss2 = (flow_diffs2 * occ_b.float()).sum(dim=1, keepdim=True).mean()
 

train.py
@@ -123,7 +123,9 @@ parser.add_argument('--weight_decay', type=float,
                     default=0.0, help='weight decay')
 parser.add_argument('--dropout', type=bool, default=False,
                     help='dropout for regularization', choices=[True, False])
-parser.add_argument('--grad_clip', type=float, default=0,
+parser.add_argument('--grad_clip_norm', type=float, default=0,
+                    help='gradient clipping threshold')
+parser.add_argument('--grad_clip_value', type=float, default=0,
                     help='gradient clipping threshold')
 
 # model params
@@ -351,8 +353,10 @@ def train_one_epoch(args, model, loss, dataloader, optimizer, augmentations, lr_
     optimizer.zero_grad()
     total_loss = loss_dict['total_loss']
     total_loss.backward()
-    if args.grad_clip > 0:
-      torch.nn.utils.clip_grad_value_(model.parameters(), args.grad_clip)
+    if args.grad_clip_norm > 0:
+      torch.nn.utils.clip_grad_norm_(model.parameters(), args.grad_clip_norm)
+    if args.grad_clip_value > 0:
+      torch.nn.utils.clip_grad_value_(model.parameters(), args.grad_clip_value)
     optimizer.step()
 
     for key in loss_dict.keys():

train_dist.py
@@ -405,8 +405,11 @@ def train_one_epoch(args, model, loss, dataloader, optimizer, augmentations, lr_
         total_loss = loss_dict['total_loss']
         assert (not torch.isnan(total_loss)), "training loss is nan... exiting..."
         total_loss.backward()
-        if args.grad_clip > 0.0:
-            torch.nn.utils.clip_grad_value_(model.parameters(), args.grad_clip)
+
+        if args.grad_clip_norm > 0:
+            torch.nn.utils.clip_grad_norm_(model.parameters(), args.grad_clip_norm)
+        if args.grad_clip_value > 0:
+            torch.nn.utils.clip_grad_value_(model.parameters(), args.grad_clip_value)
         optimizer.step()
 
         for key in loss_dict.keys():

utils/loss_utils.py
@@ -161,7 +161,7 @@ def _smoothness_motion_2nd(sf, img, beta=1):
     return (smoothness_x.abs() + smoothness_y.abs())
 
 
-def _disp2depth_kitti_K(disp, fx, min_depth=0.1, max_depth=100):
+def _disp2depth_kitti_K(disp, fx, min_depth=1e-3, max_depth=80):
 
     #scale disparity according to monodepthv2
     # min_disp = (0.54 * fx) / max_depth
@@ -173,6 +173,7 @@ def _disp2depth_kitti_K(disp, fx, min_depth=0.1, max_depth=100):
     mask = (disp > 0).float()
     depth = fx.unsqueeze(1).unsqueeze(
         1).unsqueeze(1) * 0.54 / (disp + (1.0 - mask))
+    depth = torch.clamp(depth, min_depth, max_depth)
 
     return depth
 

utils/sceneflow_util.py
@@ -40,10 +40,10 @@ def flow_horizontal_flip(flow_input):
     return flow_flip.contiguous()
 
 
-def disp2depth_kitti(pred_disp, k_value):
+def disp2depth_kitti(pred_disp, fx, min_depth=1e-3, max_depth=80):
 
-    pred_depth = k_value.unsqueeze(1).unsqueeze(1).unsqueeze(1) * 0.54 / (pred_disp + 1e-8)
-    pred_depth = torch.clamp(pred_depth, 1e-3, 80)
+    pred_depth = fx.unsqueeze(1).unsqueeze(1).unsqueeze(1) * 0.54 / (pred_disp + 1e-8)
+    pred_depth = torch.clamp(pred_depth, min_depth, max_depth)
 
     return pred_depth
 