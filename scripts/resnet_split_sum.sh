04fc920e0b25c70b1cde7cf8d10c3bacd6daf8b8
Shreyas Niradi <shreyas.niradi@gmail.com>
2020-12-31 03:16:27 -0800

    update split script to match joint

--- scripts/resnet_split_sum.sh
+++ scripts/resnet_split_sum.sh
@@ -1,25 +1,27 @@
 python3 train_dist.py --data_root="/mnt/data/kitti_jpg/" \
                  --log_root="/ceph/checkpoints/" \
-                 --exp_dir="resnet_split_sum" \
-                 --exp_name="" \
+                 --exp_dir="resnet_joint_sum" \
+                 --exp_name="v2" \
                  --log_freq=1 \
                  --save_freq=1 \
                  --dataset_name="KITTI_EIGEN" \
-                 --model_name="split" \
+                 --validate=True \
+                 --model_name='split' \
+                 --encoder_name='resnet' \
                  --num_examples=-1 \
                  --num_workers=16 \
                  --start_epoch=1 \
-                 --epochs=30 \
+                 --epochs=40 \
                  --batch_size=2 \
                  --lr=1e-4 \
                  --lr_gamma=0.5 \
                  --lr_sched_type='step' \
-                 --milestones 15 25 \
+                 --milestones 15 30 35 \
                  --flow_reduce_mode='sum' \
                  --disp_sm_w=0.1 \
                  --flow_sm_w=200 \
                  --mask_sm_w=0.0 \
                  --mask_reg_w=0.0 \
-                 --flow_pts_w=0.0 \
-                 --mask_cons_w=0.0
-                 #--use_disp_min=True \
+                 --flow_pts_w=0.2 \
+                 --mask_cons_w=0.0 \
+                 --flow_diff_thresh=1e-2
