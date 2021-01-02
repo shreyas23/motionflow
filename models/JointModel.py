631ad3d46c06e357f3d3c7c9adefa9cd624d1087
Shreyas Niradi <shreyas.niradi@gmail.com>
2021-01-01 18:17:06 -0800

    stupid update

--- models/JointModel.py
+++ models/JointModel.py
@@ -164,9 +164,8 @@ class JointModel(nn.Module):
             if l != self.output_level:
                 disp_l1 = self.sigmoid(disp_l1) * 0.3
                 disp_l2 = self.sigmoid(disp_l2) * 0.3
-                if self.use_mask:
-                    mask_l1 = self.sigmoid(mask_l1)
-                    mask_l2 = self.sigmoid(mask_l2)
+                mask_l1 = self.sigmoid(mask_l1)
+                mask_l2 = self.sigmoid(mask_l2)
                 sceneflows_f.append(flow_f)
                 sceneflows_b.append(flow_b)                
                 disps_1.append(disp_l1)
@@ -205,9 +204,8 @@ class JointModel(nn.Module):
         output_dict['flows_b'] = upsample_outputs_as(sceneflows_b[::-1], x1_rev)
         output_dict['disps_l1'] = upsample_outputs_as(disps_1[::-1], x1_rev)
         output_dict['disps_l2'] = upsample_outputs_as(disps_2[::-1], x1_rev)
-        if self.use_mask:
-            output_dict['masks_l1'] = upsample_outputs_as(masks_1[::-1], x1_rev)
-            output_dict['masks_l2'] = upsample_outputs_as(masks_2[::-1], x1_rev)
+        output_dict['masks_l1'] = upsample_outputs_as(masks_1[::-1], x1_rev)
+        output_dict['masks_l2'] = upsample_outputs_as(masks_2[::-1], x1_rev)
         output_dict["pose_f"] = poses_f[::-1]
         output_dict["pose_b"] = poses_b[::-1]
 
@@ -237,9 +235,8 @@ class JointModel(nn.Module):
                 output_dict_r['flows_b'][ii] = flow_horizontal_flip(output_dict_r['flows_b'][ii])
                 output_dict_r['disps_l1'][ii] = torch.flip(output_dict_r['disps_l1'][ii], [3])
                 output_dict_r['disps_l2'][ii] = torch.flip(output_dict_r['disps_l2'][ii], [3])
-                if self.use_mask:
-                    output_dict_r['masks_l1'][ii] = torch.flip(output_dict_r['masks_l1'][ii], [3])
-                    output_dict_r['masks_l2'][ii] = torch.flip(output_dict_r['masks_l2'][ii], [3])
+                output_dict_r['masks_l1'][ii] = torch.flip(output_dict_r['masks_l1'][ii], [3])
+                output_dict_r['masks_l2'][ii] = torch.flip(output_dict_r['masks_l2'][ii], [3])
 
             output_dict['output_dict_r'] = output_dict_r
 
@@ -268,16 +265,14 @@ class JointModel(nn.Module):
                 flow_b_pp.append(post_processing(output_dict['flows_b'][ii], flow_horizontal_flip(output_dict_flip['flows_b'][ii])))
                 disp_l1_pp.append(post_processing(output_dict['disps_l1'][ii], torch.flip(output_dict_flip['disps_l1'][ii], [3])))
                 disp_l2_pp.append(post_processing(output_dict['disps_l2'][ii], torch.flip(output_dict_flip['disps_l2'][ii], [3])))
-                if self.use_mask:
-                    mask_l1_pp.append(post_processing(output_dict['masks_l1'][ii], torch.flip(output_dict_flip['masks_l1'][ii], [3])))
-                    mask_l2_pp.append(post_processing(output_dict['masks_l2'][ii], torch.flip(output_dict_flip['masks_l2'][ii], [3])))
+                mask_l1_pp.append(post_processing(output_dict['masks_l1'][ii], torch.flip(output_dict_flip['masks_l1'][ii], [3])))
+                mask_l2_pp.append(post_processing(output_dict['masks_l2'][ii], torch.flip(output_dict_flip['masks_l2'][ii], [3])))
 
             output_dict['flows_f_pp'] = flow_f_pp
             output_dict['flows_b_pp'] = flow_b_pp
             output_dict['disps_l1_pp'] = disp_l1_pp
             output_dict['disps_l2_pp'] = disp_l2_pp
-            if self.use_mask:
-                output_dict['masks_l1_pp'] = disp_l1_pp
-                output_dict['masks_l2_pp'] = disp_l2_pp
+            output_dict['masks_l1_pp'] = disp_l1_pp
+            output_dict['masks_l2_pp'] = disp_l2_pp
 
         return output_dict
--- models/joint_decoders.py
+++ models/joint_decoders.py
@@ -22,8 +22,7 @@ class JointDecoder(nn.Module):
         self.conv_d1 = Conv(32, 1, nonlin='none')
         self.conv_pose = Conv(32, num_refs * 6, kernel_size=1, nonlin='none')
 
-        if self.use_mask:
-            self.conv_mask = Conv(32, 1, nonlin='none')
+        self.conv_mask = Conv(32, 1, nonlin='none')
 
     def forward(self, x):
         x_out = self.convs(x)
@@ -32,10 +31,7 @@ class JointDecoder(nn.Module):
         pose_out = self.conv_pose(x_out)
         pred_pose = pose_out.mean(3).mean(2) * 0.1
 
-        if self.use_mask:
-            mask = self.conv_mask(x_out)
-        else:
-            mask = None
+        mask = self.conv_mask(x_out)
 
         return x_out, sf, disp1, mask, pred_pose, pose_out
 
@@ -63,11 +59,10 @@ class JointContextNetwork(nn.Module):
 
         self.conv_pose = Conv(32, num_refs * 6, kernel_size=1, nonlin='none')
 
-        if self.use_mask:
-            self.conv_mask = nn.Sequential(
-                Conv(32, 1, nonlin='none'),
-                torch.nn.Sigmoid()
-            )
+        self.conv_mask = nn.Sequential(
+            Conv(32, 1, nonlin='none'),
+            torch.nn.Sigmoid()
+        )
 
     def forward(self, x):
 
@@ -77,9 +72,6 @@ class JointContextNetwork(nn.Module):
         pose_out = self.conv_pose(x_out)
         pred_pose = pose_out.mean(3).mean(2) * 0.1
 
-        if self.use_mask:
-            mask = self.conv_mask(x_out)
-        else:
-            mask = None
+        mask = self.conv_mask(x_out)
 
         return sf, disp1, pred_pose, mask
