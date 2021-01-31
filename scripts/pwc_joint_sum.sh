f53b59bae2cf5ac8b5f6979505c67d4bc97489cd
Shreyas Niradi <shreyas.niradi@gmail.com>
2021-01-30 18:47:28 -0800

    lil fix

--- scripts/pwc_joint_sum.sh
+++ scripts/pwc_joint_sum.sh
@@ -10,13 +10,12 @@ python3 train_dist.py --data_root="/mnt/data/kitti_jpg/" \
                       --encoder_name='pwc' \
                       --num_examples=-1 \
                       --num_workers=16 \
-                      --start_epoch=1 \
-                      --epochs=20 \
+                      --start_epoch=16 \
+                      --epochs=5 \
                       --batch_size=4 \
-                      --lr=3e-4 \
+                      --lr=1.5e-4 \
                       --lr_gamma=0.5 \
-                      --lr_sched_type='step' \
-                      --milestones 7 \
+                      --lr_sched_type='none' \
                       --flow_reduce_mode='sum' \
                       --disp_sm_w=0.1 \
                       --flow_sm_w=200 \
