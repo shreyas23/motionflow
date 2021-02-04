c2d7b28c57d7c7d18fc2fcc94b40524bf11d170f
Shreyas Niradi <shreyas.niradi@gmail.com>
2021-02-04 03:57:47 -0800

    small fix for optimizer

--- train_dist.py
+++ train_dist.py
@@ -190,7 +190,7 @@ def train(gpu, args):
             optimizer, milestones=args.milestones, gamma=args.lr_gamma)
     elif args.lr_sched_type == 'cyclic':
         lr_scheduler = torch.optim.lr_scheduler.CyclicLR(
-            optimizer, base_lr=5e-5, max_lr=3e-4,step_size_up=5,mode="triangular2")
+            optimizer, base_lr=5e-5, max_lr=3e-4,step_size_up=5,mode="triangular2", cycle_momentum=False)
     elif args.lr_sched_type == 'none':
         lr_scheduler = None
 
