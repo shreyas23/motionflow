e02d4ba592d0c4f4ef94e0f96475ef93b7b1027c
Shreyas Niradi <shreyas.niradi@gmail.com>
2021-02-04 17:52:07 -0800

    test bs

--- scripts/pwc_joint_sum.sh
+++ scripts/pwc_joint_sum.sh
@@ -1,8 +1,8 @@
 python3 train_dist.py --data_root="/mnt/data/kitti_jpg/" \
                       --test_data_root="/mnt/data/kitti_jpg/kitti2015/" \
                       --log_root="/ceph/checkpoints/" \
-                      --exp_dir="pwc_joint_sum" \
-                      --exp_name="v2_no_ptsfb/" \
+                      --exp_dir="pwc_joint_sum_lr" \
+                      --exp_name="pwc_4e4" \
                       --log_freq=1 \
                       --save_freq=1 \
                       --dataset_name="KITTI_EIGEN" \
@@ -11,16 +11,16 @@ python3 train_dist.py --data_root="/mnt/data/kitti_jpg/" \
                       --num_examples=-1 \
                       --num_workers=16 \
                       --start_epoch=1 \
-                      --epochs=30 \
+                      --epochs=20 \
                       --batch_size=4 \
-                      --lr=2e-4 \
+                      --lr=4e-4 \
                       --lr_gamma=0.5 \
                       --lr_sched_type='step' \
-                      --milestones 20 \
+                      --milestones 15 \
                       --flow_reduce_mode='sum' \
                       --disp_sm_w=0.1 \
                       --flow_sm_w=200 \
-                      --mask_sm_w=0.1 \
+                      --mask_sm_w=0.05 \
                       --mask_reg_w=0.2 \
                       --flow_pts_w=0.2 \
                       --flow_cycle_w=0.0 \
