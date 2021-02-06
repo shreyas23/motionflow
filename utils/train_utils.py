b218582988a15512d1092139abdd52bf9437f251
Shreyas Niradi <shreyas.niradi@gmail.com>
2021-02-06 15:58:24 -0800

    lil fix

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -234,13 +234,13 @@ def visualize_output(args, input_dict, output_dict, epoch, writer, prefix):
         disp_l2 = interpolate2d_as(output_dict['disps_l2_pp'][0].detach(), img_l1)
         sf_b = interpolate2d_as(output_dict['flows_b_pp'][0].detach(), img_l1)
         if use_mask:
-            mask_l2 = interpolate2d_as(output_dict['masks_l2'][0].detach(), img_l1)
+            mask_l2 = interpolate2d_as(output_dict['masks_l2_pp'][0].detach(), img_l1)
     else:
         disp_l1 = interpolate2d_as(output_dict['disps_l1'][0].detach(), img_l1)
         disp_l2 = interpolate2d_as(output_dict['disps_l2'][0].detach(), img_l1)
         sf_b = interpolate2d_as(output_dict['flows_b'][0].detach(), img_l1)
         if use_mask:
-            mask_l2 = interpolate2d_as(output_dict['masks_l2_pp'][0].detach(), img_l1)
+            mask_l2 = interpolate2d_as(output_dict['masks_l2'][0].detach(), img_l1)
 
     if 'pose_b' in output_dict:
         poses_b = output_dict['pose_b']
