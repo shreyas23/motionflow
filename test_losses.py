ad1d546e71eb789556ab5bf2d06fb10befbfa56d
Shreyas Niradi <shreyas.niradi@gmail.com>
2021-02-22 17:11:02 -0800

    test mask weight * pose_weight

--- test_losses.py
+++ test_losses.py
@@ -344,7 +344,7 @@ class Loss_SceneFlow_SelfSup(nn.Module):
         total_loss = loss_sf_sum * f_weight + \
                      loss_dp_sum * d_weight + \
                      loss_pose_sum * f_weight + \
-                     loss_mask_sum + \
+                     loss_mask_sum * f_weight + \
                      loss_cons_sum * self.static_cons_w + \
                      loss_feat_disc_sum * self.feat_sm_w + \
                      loss_feat_sm_sum * self.feat_sm_w
