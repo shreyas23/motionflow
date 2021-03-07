581ffe96b4f27da3e082e976d2a886868746efc1
Shreyas Niradi <shreyas.niradi@gmail.com>
2021-03-06 21:29:55 -0800

    small fix

--- test_losses.py
+++ test_losses.py
@@ -316,7 +316,7 @@ class Loss_SceneFlow_SelfSup(nn.Module):
         total_loss = loss_sf_sum * f_weight + \
                      loss_dp_sum * d_weight + \
                      loss_pose_sum * f_weight + \
-                     loss_mask_sum * + \
+                     loss_mask_sum * \
                      loss_cons_sum * self.static_cons_w 
 
         loss_dict = {}
